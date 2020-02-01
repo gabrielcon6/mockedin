56b4cb34dc3820d497a8af8baed4a71e9ed644b1
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-02-01 13:46:06 +1100

    connecting backedn

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -27,11 +38,12 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://gabriel:mern@cluster0-2r7ns.mongodb.net/mern?retryWrites=true&w=majority')
+  .connect(
+    'mongodb+srv://gabriel:mern@cluster0-2r7ns.mongodb.net/mern?retryWrites=true&w=majority'
+      )
   .then(() => {
     app.listen(5000);
   })
   .catch(err => {
     console.log(err);
   });
-
