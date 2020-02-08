a1b337175502fa82a4b7eae54d3f4c997099908a
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-02-08 14:19:07 +1100

    test

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-2r7ns.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    process.env.DB_URL, {useNewUrlParser: true}
   )
   .then(() => {
     const port = process.env.PORT;
