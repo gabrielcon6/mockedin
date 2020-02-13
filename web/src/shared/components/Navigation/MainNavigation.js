b2cd6c2a2e22c7e754f7581bf1af7b596f8545ee
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-02-14 00:26:37 +1100

    fixed logo

--- web/src/shared/components/Navigation/MainNavigation.js
+++ web/src/shared/components/Navigation/MainNavigation.js
@@ -38,7 +38,7 @@ const MainNavigation = props => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">MockedIn</Link>
+          {/* <Link to="/">MockedIn</Link> */}
            
           <Link to={`/${props.userId}/profile`}>MockedIn</Link>
         </h1>
