30301c2dc30f717f8732c5d3d9b045a738dd67c5
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-02-13 18:49:56 +1100

    mockedin logo as profile button

--- web/src/shared/components/Navigation/MainNavigation.js
+++ web/src/shared/components/Navigation/MainNavigation.js
@@ -37,7 +37,7 @@ const MainNavigation = props => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">MockedIn</Link>
+          <Link to={`/${props.userId}/profile`}>MockedIn</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
