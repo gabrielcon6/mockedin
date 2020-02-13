263c3f74fcf4bedb763e625f5953414c065d8d19
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-02-14 02:13:02 +1100

    fixed location bug - now it does not disappear

--- web/src/profile/header/pages/UpdateHeader.js
+++ web/src/profile/header/pages/UpdateHeader.js
@@ -142,7 +142,7 @@ const UpdateHeader = () => {
     formData.append('name', formState.inputs.name.value);
     // formData.append('file', formState.inputs.file.value);
     formData.append('jobTitle', formState.inputs.jobTitle.value);
-    formData.append('location', address);
+    formData.append('location', address ? address : loadedHeader.location);
     formData.append('about', formState.inputs.about.value);
     formData.append('adminComments', formState.inputs.adminComments.value);
     formData.append('isOk', check);
@@ -214,6 +214,7 @@ const UpdateHeader = () => {
             onChange={setAddress}
             onSelect={handleSelect}
             searchOptions={searchOptions}
+            initialValue={loadedHeader.location}
           >
             {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
               <div>
