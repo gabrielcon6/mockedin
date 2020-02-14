6f07428e55d2dd80e11dc6030ca211e3ae0b928c
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2020-02-14 19:11:59 +1100

    hard coded progress bar working

--- web/src/shared/components/UIElements/ProgressBar.js
+++ web/src/shared/components/UIElements/ProgressBar.js
@@ -1,78 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CardMockedin from '../../../shared/components/UIElements/CardMockedin'
 
 import { FaStar, FaPoo } from 'react-icons/fa';
 import './ProgressBar.css';
 
-// var progress = 25;
-
-// var wrapper_elt = document.querySelector('.progress-wrap');
-// var progress_elt = document.querySelector('.progress-bar');
-
-// //resize
-// var width = wrapper_elt.getBoundingClientRect().width;
-
-// var update = function(progress) {
-//   var percent = progress / 100;
-//   var total = percent * width;
-  
-//   progress_elt.style.left = Math.floor(total)+'px';
-// };
-
-// var make_loop = function(val, duration) {
-//   var current = 0;
-//   var tsinc = duration / val;
-//   var loop = function() {
-//     if( current <= val ) {
-//       update(current++);
-//       window.setTimeout(loop, tsinc);
-//     }
-//   }
-//   loop();
-// };
-
-// make_loop(60, 2000);
 
 const ProgressBar = props => {
+    const [progress, setProgres] = useState(0)
 
-const storedData = JSON.parse(localStorage.getItem('userData'));
-const isAdmin = storedData.isAdmin
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    const isAdmin = storedData.isAdmin
 
-  return (
-    <CardMockedin className="cardMocke">
+    const handleScore = (e) => {
+        let score = parseInt(e.target.value)*25
+        setProgres(score)
+    }
 
-        <div className='progress-bar-title'>
-            <h2>Profile strengh: </h2>
-        </div>
-        <div className='strenght-bar'>
-            <div className='strengh-bar__element'>
-                <div class="progress-wrap progress">
-                    <div class="progress-bar progress" style={{left:'100%'}}></div>
-                   
-                </div>
-                <div className='strenght-bar-star'>
-                        <FaPoo style={{color: 'rgb(100,70,41)'}}/>
-                        <FaStar style={{color:'rgb(238,184,44)'}}/>
+    return (
+        <CardMockedin className="cardMocke">
+
+            <div className='progress-bar-title'>
+                <h2>Profile strengh: </h2>
+            </div>
+            <div className='strenght-bar'>
+                <div className='strengh-bar__element'>
+                    <div class="progress-wrap progress">
+                        <div class="progress-bar progress" style={{left: progress + '%'}}></div>
+                    
                     </div>
-                
+                    <div className='strenght-bar-star'>
+                            <FaPoo style={{color: 'rgb(100,70,41)'}}/>
+                            <FaStar style={{color:'rgb(238,184,44)'}}/>
+                        </div>
+                    
+                </div>
             </div>
-        </div>
-        {isAdmin && (
-        <div className='select-element'>
-            <div class="select">
-            <select name="slct" id="slct">
-                <option selected disabled>Rate this profile</option>
-                <option value="1">Bad</option>
-                <option value="2">Meh...</option>
-                <option value="3">Good.</option>
-                <option value="4">Great!!</option>
-            </select>
+            {isAdmin && (
+            <div className='select-element'>
+                <div class="select">
+                <select name="slct" id="slct" onChange={handleScore}>
+                    <option selected disabled>Rate this profile</option>
+                    <option value="1">Bad</option>
+                    <option value="2">Meh...</option>
+                    <option value="3">Good.</option>
+                    <option value="4">Great!!</option>
+                </select>
+                </div>
             </div>
-        </div>
-        )}
-        <br />
-    </CardMockedin>
-  );
+            )}
+            <br />
+        </CardMockedin>
+    );
 };
 
 export default ProgressBar;
